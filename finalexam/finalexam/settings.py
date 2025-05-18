4ec64352fea9964f7b9238089f35b257b6c0eeda
amzynK <kooqhoo@gmail.com>
2025-05-18 19:23:38 +0700

    home, about, css

--- finalexam/finalexam/settings.py
+++ finalexam/finalexam/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'finalexam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finex',
+        'USER': 'postgres',
+        'PASSWORD': 'useme',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- finalexam/finalexampredict/views.py
+++ finalexam/finalexampredict/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("")
+    return render(request,"finalexampredict/home.html")
+
+def about(request):
+    return render(request,"finalexampredict/about.html")
